567769f8bbf001cabff218842df82b3ab256c327
sjacob210 <sjacob210@gmail.com>
2018-10-10 14:58:11 -0400

    some updates

--- Scripts/plate3_combined.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=cpu_long
-
-sbatch --partition=cpu_medium --wait /gpfs/scratch/sj1879/REDUX/Scripts/FastQC_Scripts/Find_Texts_For_FQC.sh /gpfs/scratch/sj1879/REDUX/Manifests/Plate3_Merged/
-/gpfs/scratch/sj1879/REDUX/Scripts/GeneralScripts/Change_Auth.sh
-sbatch --partition=cpu_medium  /gpfs/scratch/sj1879/REDUX/Scripts/FastQC_Scripts/Loop_FastQC_MultiQC.sh /gpfs/scratch/sj1879/REDUX/Manifests/Plate3_Merged/FASTQC/
-
-
-
