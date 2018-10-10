4c4b2410a88819e239922d73b3cd3b8f1e1e562c
Samson <sjacob210@gmail.com>
2018-10-10 14:57:08 -0400

    Updates

--- Scripts/Loop_Submit_FastQC_pipeline.sh
+++ Scripts/Loop_Submit_FastQC_pipeline.sh
@@ -2,7 +2,7 @@
 
 #mkdir $1/Manifests
 for fol in $(find $1/Manifests/* -maxdepth 0 -name "P*"  -type d) ;do
-    sbatch --partition=cpu_medium --wait /gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Find_Texts_For_FQC.sh $fol
-/gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Change_Auth.sh $fol
-sbatch --partition=cpu_medium  /gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Loop_FastQC_MultiQC.sh $fol/FASTQC/
+    sbatch --partition=cpu_medium --wait $1/Scripts/Find_Texts_For_FQC.sh $fol
+$1/Scripts/Change_Auth.sh $fol
+sbatch --partition=cpu_medium  $1/Scripts/Loop_FastQC_MultiQC.sh $fol/FASTQC/
 done
