905a2c34c98366c01ca7bdc9c4f5b4849739cd16
Samson <sjacob210@gmail.com>
2018-10-10 14:55:29 -0400

    De-Identify

--- Scripts/loop_Multiqc.sh
+++ Scripts/loop_Multiqc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for fol in $(find $1/Manifests/P* -name "*FASTQC" -type d);do
-	sbatch --partition=cpu_medium --wait /gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/run_multiqc.sh $fol
+	sbatch --partition=cpu_medium --wait $1/Scripts/run_multiqc.sh $fol
 done
