1ea71adc2809b0184130599acdcac8942d2f505c
Samson <sjacob210@gmail.com>
2018-10-10 14:20:27 -0400

    update

--- Find_Texts_For_FQC.sh
+++ Find_Texts_For_FQC.sh
@@ -11,12 +11,11 @@ resultspath=$1/FASTQC/
 
 mkdir -p $resultspath
 
-/gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Change_Auth.sh $resultspath
+$1/Scripts/Change_Auth.sh $resultspath
 
 R1_pth="$(find $1 -name "*R1*.txt" -type f)"
 R2_pth="$(find $1 -name "*R2*.txt" -type f)"
 
-srun /gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Run_FASTQC_FromPath.sh $R1_pth $R2_pth $resultspath
-
-/gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Change_Auth.sh $resultspath
+srun $1/Scripts/Run_FASTQC_FromPath.sh $R1_pth $R2_pth $resultspath
+$1/Scripts/Change_Auth.sh $resultspath
 
