58ca942b1fabf1b1b1151b4bab7389f91670d49e
Samson <sjacob210@gmail.com>
2018-10-10 14:54:46 -0400

    Updated with comments

--- Scripts/Loop_Find_Fastqs.sh
+++ Scripts/Loop_Find_Fastqs.sh
@@ -7,17 +7,19 @@
 #SBATCH --job-name=LoopThroughDir
 
 #make sure Manifests subfolder is main
-parentloc=/gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Manifests/
+parentloc=$1/Manifests/
+
 #create directory if need be
 if [ !  -d "$parentloc" ];then
 	mkdir -p $parentloc
 fi
 
-/gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Change_Auth.sh $parentloc
+# clear permissions
+$1/Scripts/Change_Auth.sh $parentloc
 
-
+# note the pseudo-regex P*
 for fol in $(find $1/* -maxdepth 0 -name "P*"  -type d); do
-	platename="${fol##*/}"
-	/gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Find_Mates_From_Dir.sh $fol $platename 
-	/gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Change_Auth.sh $parentloc
+	platename="${fol##*/}" # string manipulate to get the plate name
+	$1/Scripts/Find_Mates_From_Dir.sh $fol $platename  # Find paired information
+	$1/Scripts/Change_Auth.sh $parentloc # change permissions
 done
