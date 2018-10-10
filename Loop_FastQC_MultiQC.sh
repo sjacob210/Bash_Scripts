58bcaab586ba23785498dbf3ceb8c70b4a0a3f3a
Samson <sjacob210@gmail.com>
2018-10-10 14:25:54 -0400

    Updates to Anon

--- Loop_FastQC_MultiQC.sh
+++ Loop_FastQC_MultiQC.sh
@@ -7,21 +7,21 @@
 
 
 # create manifest directory at top level
-srun /gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Loop_Find_Fastqs.sh $1
+srun $1/Scripts/Loop_Find_Fastqs.sh $1
 
 # change the directory to 777
-/gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Change_Auth.sh $1
+srun $1/Scripts/Change_Auth.sh $1
 wait
 
 # run the FastQC pipeline
-sbatch --wait --partition=cpu_medium /gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Loop_Submit_FastQC_pipeline.sh $1
+sbatch --wait --partition=cpu_medium $1/Scripts/Loop_Submit_FastQC_pipeline.sh $1
 
 #change auth
-srun /gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Change_Auth.sh $1
+srun $1/Scripts/Change_Auth.sh $1
 wait
 
 #run multiqc
-sbatch --wait --partition=cpu_medium /gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/loop_Multiqc.sh $1
+sbatch --wait --partition=cpu_medium $1/Scripts/loop_Multiqc.sh $1
 wait
 
 
@@ -29,15 +29,16 @@ wait
 sbatch $1/Multiqc_Pipeline.sh $1
 
 # generate the MultiQC aggregated results
-#module load python/cpu/2.7.15
-#python Find_MultiQC_and_Agg.py $1/Manifests/
 
-#/gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Change_Auth.sh $1
+module load python/cpu/2.7.15
+python Find_MultiQC_and_Agg.py $1/Manifests/
+
+$1/Scripts/Change_Auth.sh $1
 
 #generate report 
-#python MultiQC_generic.py $1/Manifests/MULTIQC_AGGREGATED_RESULTS.csv
+python MultiQC_generic.py $1/Manifests/MULTIQC_AGGREGATED_RESULTS.csv
 
-#/gpfs/data/proteomics/projects/TRASANDE_LAB/Meta_Data_Information/FASTQC/MERGEDFASTQS_2/Change_Auth.sh $1
+$1/Scripts/Change_Auth.sh $1
 
 #clear slurm output
 rm -f $1/slurm*.out
